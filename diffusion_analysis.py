b51e9c170e78905dfea1f5daffe924a35aa0c071
Tyler Reddy <tyler.je.reddy@gmail.com>
2014-09-19 14:02:13 +0100

    -diffusion_analysis.py: -work on fit_anomalous_diffusion_data() function: 	-now extracts and returns the fractional diffusion coefficient, alpha, and their standard deviations (instead of returning the scipy curve_fit data arrays and expecting the user to unpack those) 	-some improvements to docstring

--- diffusion_analysis.py
+++ diffusion_analysis.py
@@ -5,7 +5,19 @@ The purpose of this Python module is to provide utility functions for analyzing 
 
 def fit_anomalous_diffusion_data(time_data_array,MSD_data_array,degrees_of_freedom=2):
     '''This function should fit anomalous diffusion data to Equation 1 in Kneller et al. (2011) J Chem Phys 135: 141105. It will assign an appropriate coefficient based on the specified degrees_of_freedom. The latter value defaults to 2 (i.e., a planar phospholipid bilayer).
-    Input data should include arrays of MSD (in Angstroms) and time values (in ns).'''
+    Input data should include arrays of MSD (in Angstroms) and time values (in ns).
+    The results are returned in a tuple.
+    Returns
+    -------
+    fractional_diffusion_coefficient
+        The fractional diffusion coefficient (units of Angstrom ** 2 / ns ** alpha)
+    standard_deviation_fractional_diffusion_coefficient
+        The standard deviation of the fractional diffusion coefficent (units of Angstrom ** 2 / ns ** alpha)
+    alpha
+        The scaling exponent (no dimensions) of the non-linear fit
+    standard_deviation_alpha
+        The standard deviation of the scaling exponent (no dimensions)
+        '''
 
     def function_to_fit(time,fractional_diffusion_coefficient,scaling_exponent):
         coefficient_dictionary = {1:2,2:4,3:6} #dictionary for mapping degrees_of_freedom to coefficient in fitting equation
@@ -19,7 +31,15 @@ def fit_anomalous_diffusion_data(time_data_array,MSD_data_array,degrees_of_freed
     sample_fitting_data_Y_values_Angstroms = function_to_fit(sample_fitting_data_X_values_nanoseconds, *optimized_parameter_value_array)
     #could then plot the non-linear fit curve in matplotlib with, for example: axis.plot(sample_fitting_data_X_values_nanoseconds,sample_fitting_data_Y_values_Angstroms,color='black')
     #could plot the original data points alongside the fit (MSD vs time) with, for example: axis.scatter(time_data_array,MSD_data_array,color='black')
-    return (optimized_parameter_value_array, estimated_covariance_params_array,sample_fitting_data_X_values_nanoseconds,sample_fitting_data_Y_values_Angstroms)
+
+    #extract pertinent values from the scipy curve_fit arrays (D_alpha, alpha, and their standard deviations)
+    parameter_standard_deviation_array = numpy.sqrt(numpy.diagonal(estimated_covariance_params_array))
+    fractional_diffusion_coefficient = optimized_parameter_value_array[0]
+    standard_deviation_fractional_diffusion_coefficient = parameter_standard_deviation_array[0]
+    alpha = optimized_parameter_value_array[1]
+    standard_deviation_alpha = parameter_standard_deviation_array[1]
+
+    return (fractional_diffusion_coefficient, standard_deviation_fractional_diffusion_coefficient, alpha, standard_deviation_alpha,sample_fitting_data_X_values_nanoseconds,sample_fitting_data_Y_values_Angstroms)
 
 def fit_linear_diffusion_data(time_data_array,MSD_data_array,index_window_filter):
     '''The (crude) linear MSD vs. time slope = diffusion constant approach and the error estimate used by g_msd.'''
