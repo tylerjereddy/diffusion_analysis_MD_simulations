107438b15c579c7715e51b66acb5eb920141c969
Tyler Reddy <tyler.je.reddy@gmail.com>
2014-09-19 13:21:49 +0100

    -diffusion_analysis.py: -work on fit_anomalous_diffusion_data() function: 	-removed plot colour details for the non-linear fitting plot as they were system-specific 	-removed execution of plotting commands as the plotting component of the workflow shoud be left up to the user; I want this to be a general function that just produces the data 	-adjusted code to account for degrees of freedom in the calculation (default is 2 --> typical for membrane bilayers)

--- diffusion_analysis.py
+++ diffusion_analysis.py
@@ -3,22 +3,23 @@ Author: Tyler Reddy
 
 The purpose of this Python module is to provide utility functions for analyzing the diffusion of particles in molecular dynamics simulation trajectories using either linear or anomalous diffusion models.'''
 
-def fit_anomalous_diffusion_data(time_data_array,MSD_data_array,axis,species_label,species_type):
-    '''This function should fit anomalous diffusion data to Equation 1 in Kneller et al. (2011) J Chem Phys 135: 141105. 
-    Input data should include arrays of MSD and time values.'''
+def fit_anomalous_diffusion_data(time_data_array,MSD_data_array,degrees_of_freedom=2):
+    '''This function should fit anomalous diffusion data to Equation 1 in Kneller et al. (2011) J Chem Phys 135: 141105. It will assign an appropriate coefficient based on the specified degrees_of_freedom. The latter value defaults to 2 (i.e., a planar phospholipid bilayer).
+    Input data should include arrays of MSD (in Angstroms) and time values (in ns).'''
+
     def function_to_fit(time,fractional_diffusion_coefficient,scaling_exponent):
-        return 4.0 * fractional_diffusion_coefficient * (time ** scaling_exponent) #equation 1 in the above paper
+        coefficient_dictionary = {1:2,2:4,3:6} #dictionary for mapping degrees_of_freedom to coefficient in fitting equation
+        coefficient = coefficient_dictionary[degrees_of_freedom]
+        return coefficient * fractional_diffusion_coefficient * (time ** scaling_exponent) #equation 1 in the above paper with appropriate coefficient based on degrees of freedom
+
+    #fit the above function to the data and pull out the resulting parameters
     optimized_parameter_value_array, estimated_covariance_params_array = scipy.optimize.curve_fit(function_to_fit,time_data_array,MSD_data_array)
-    #test plot the resulting fit as well:
-    if species_type == 'protein':
-        color_dict = {'HA':'red','NA':'blue','M2':'purple'} #to match the linear MSD vs. time data for simpler comparison
-    elif species_type == 'lipid':
-        color_dict = {'POPS':'blue','DOPE':'red','CHOL':'purple','PPCH':'black','DOPX':'green','FORS':'cyan','DPPC':'grey','DUPC':'brown'}
-    test_X = numpy.linspace(time_data_array[0],time_data_array[-1],100)
-    test_Y = function_to_fit(test_X, *optimized_parameter_value_array)
-    axis.plot(test_X,test_Y,color=color_dict[species_label])
-    axis.scatter(time_data_array,MSD_data_array,color=color_dict[species_label],label=species_label)
-    return (optimized_parameter_value_array, estimated_covariance_params_array)
+    #generate sample fitting data over the range of time window values (user can plot if they wish)
+    sample_fitting_data_X_values_nanoseconds = numpy.linspace(time_data_array[0],time_data_array[-1],100)
+    sample_fitting_data_Y_values_Angstroms = function_to_fit(sample_fitting_data_X_values_nanoseconds, *optimized_parameter_value_array)
+    #could then plot the non-linear fit curve in matplotlib with, for example: axis.plot(sample_fitting_data_X_values_nanoseconds,sample_fitting_data_Y_values_Angstroms,color='black')
+    #could plot the original data points alongside the fit (MSD vs time) with, for example: axis.scatter(time_data_array,MSD_data_array,color='black')
+    return (optimized_parameter_value_array, estimated_covariance_params_array,sample_fitting_data_X_values_nanoseconds,sample_fitting_data_Y_values_Angstroms)
 
 def fit_linear_diffusion_data(time_data_array,MSD_data_array,index_window_filter):
     '''The (crude) linear MSD vs. time slope = diffusion constant approach and the error estimate used by g_msd.'''
