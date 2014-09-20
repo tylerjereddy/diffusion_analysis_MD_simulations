73b97a946916b2a78695aca0810ea2a5127a36f0
Tyler Reddy <tyler.je.reddy@gmail.com>
2014-09-20 18:21:38 +0100

    -diffusion_analysis.py: 	-fit_anomalous_diffusion_data() function: 		-
    	-fit_linear_diffusion_data() function:
    		-
    
    -test_diffusion_analysis.py:
    	-implemented unit tests for non-linear diffusion fit alpha value trends (super & sub diffusion)

--- test_diffusion_analysis.py
+++ test_diffusion_analysis.py
@@ -12,11 +12,17 @@ class Test_anomalous_diffusion_non_linear_fit(unittest.TestCase):
         self.linearly_increasing_time_array = numpy.arange(50)
         self.linear_MSD_array_for_slope_of_two = self.linearly_increasing_time_array * 2
         self.linear_MSD_array_for_slope_of_ten = self.linearly_increasing_time_array * 10
+        self.MSD_array_subdiffusive = numpy.copy(self.linear_MSD_array_for_slope_of_two)
+        self.MSD_array_subdiffusive[-1] = self.MSD_array_subdiffusive[-1] - (self.MSD_array_subdiffusive[-1] * 0.1) #final point reduced by 10%
+        self.MSD_array_superdiffusive = numpy.copy(self.linear_MSD_array_for_slope_of_two)
+        self.MSD_array_superdiffusive[-1] = self.MSD_array_superdiffusive[-1] + (self.MSD_array_superdiffusive[-1] * 0.1) #final point increased by 10%
 
     def tearDown(self):
         del self.linearly_increasing_time_array
         del self.linear_MSD_array_for_slope_of_two
         del self.linear_MSD_array_for_slope_of_ten
+        del self.MSD_array_subdiffusive
+        del self.MSD_array_superdiffusive
 
     def test_fractional_diffusion_constant_calculation_linear_data_nonlinear_function(self):
         '''Since linear diffusion is merely a special case of the anomalous diffusion equation, the anomalous diffusion code should return the correct values given linear input data.'''
@@ -33,6 +39,15 @@ class Test_anomalous_diffusion_non_linear_fit(unittest.TestCase):
             D_expected = 10. / coefficient #simple linear D
             self.assertEqual(fractional_diffusion_coefficient,D_expected)
             self.assertEqual(alpha,alpha_expected)
+            
+    def test_scaling_exponent_trends_nonlinear_function(self):
+        '''Ensure that alpha is > 1 when fed superdiffusive data and alpha < 1 when fed subdiffusive data.'''
+        for degrees_of_freedom in [1,2,3]:
+            calculated_subdiffusive_alpha = diffusion_analysis.fit_anomalous_diffusion_data(self.linearly_increasing_time_array,self.MSD_array_subdiffusive,degrees_of_freedom=degrees_of_freedom)[2]
+            self.assertLess(calculated_subdiffusive_alpha,1.0)
+            calculated_superdiffusive_alpha = diffusion_analysis.fit_anomalous_diffusion_data(self.linearly_increasing_time_array,self.MSD_array_superdiffusive,degrees_of_freedom=degrees_of_freedom)[2]
+            self.assertGreater(calculated_superdiffusive_alpha,1.0)
+
 
 
 class Test_linear_fit_normal_diffusion(unittest.TestCase):
