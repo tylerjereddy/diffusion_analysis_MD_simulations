482e32ca294b6ca6a1b211f4b20065d622a3b01d
Tyler Reddy <tyler.je.reddy@gmail.com>
2015-12-17 15:29:57 +0000

    Added a simple Distutils setup.py script.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='diffusion_analysis',
+      version='0.1',
+      description='Diffusion analysis in MD simulations.',
+      author='Tyler Reddy, Anna Duncan',
+      url='https://github.com/tylerjereddy/diffusion_analysis_MD_simulations',
+      py_modules=['diffusion_analysis'],
+      )
